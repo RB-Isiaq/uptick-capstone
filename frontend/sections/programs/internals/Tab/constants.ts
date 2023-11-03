4eb0b3add8cd1e0552525f8b16b14490b61623c3
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-11-04 00:09:34 +0100

    feat/programs update

--- frontend/sections/programs/internals/Tab/constants.ts
+++ frontend/sections/programs/internals/Tab/constants.ts
@@ -1,6 +1,6 @@
 export const programTabs = [
-    {id: 1, title: 'Beginner'},
-    {id: 2, title: 'Tech'},
-    {id: 3, title: 'Business'},
-    {id: 4, title: 'Map'},
-]
+  { id: 1, title: 'Beginner' },
+  { id: 2, title: 'Tech' },
+  { id: 3, title: 'Business' },
+  { id: 4, title: 'Map' },
+];
